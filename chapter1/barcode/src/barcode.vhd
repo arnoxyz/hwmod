9dfb80818553f940c241d75604a3d0b01b2161e8
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-06 21:54:24 +0200

    chapter1 barcode, cleanup, just draw basic bar, todos for later stuff

--- chapter1/barcode/src/barcode.vhd
+++ chapter1/barcode/src/barcode.vhd
@@ -11,27 +11,56 @@ entity barcode is
 end entity;
 
 architecture arch of barcode is
+	-- Stuff I care later about: (make the drawing look nice)
+	-- TODO: adjust drawing window size: Initialize drawing window (having width / 10 as top and bottom margin looks nice,  / 10 works as bar height)
+	-- TODO: adjust windows_width -> determine based on input string, 400 ist just a placeholder
+	-- TODO: adjust windows_height -> Calculate based on window width
 
 begin
 	barcode_maker : process
-		constant bar_width : natural := 2;                -- Width of a single bar ("module" in Wikipedia article)
-		constant quiet_zone : natural := 15 * bar_width;  -- "quite zone" of the code (a bit more than in the Wikipedia article)
-		constant input_str: string := "HW_MOD 2024W";
-
+		-- basic drawing stuff init
 		variable vhdldraw : vhdldraw_t;
+		variable y_pos : natural;
+		variable x_pos : natural; 
 
-		variable bar_height : natural;           -- Calculate based on window width
-		variable width : natural := 400;         -- determine based on input string, 400 ist just a placeholder
-		variable y_pos : natural;                -- y position for the barcode bars
-		variable x_pos : natural := quiet_zone;  -- x position for drawing
+		-- draw Window
+		variable window_width  : natural := 400; 
+		variable window_height : natural := 400;          
+
+		constant input_str: string := "A"; 
+		constant bar_width : natural := 2; -- defined in wiki (modulo)
+		variable bar_height : natural := window_height;           
+
+		-- widths of different bar-zones:
+		constant start_symbol : natural 	:= 11 * bar_width; 
+		constant symbol_width : natural 	:= 11 * bar_width; 
+		constant quiet_zone   : natural 	:= 15 * bar_width;  
+		constant check_width  : natural 	:= 11 * bar_width;
+		constant stop_width   : natural 	:= 15 * bar_width; 
 
 	begin
-		-- Initialize drawing window (having width / 10 as top and bottom margin looks nice,  / 10 works as bar height)
-		vhdldraw.init(width, 6 * width / 10); -- This is just a dummy initialization -> adjust for total barcode width
+		vhdldraw.init(window_width, window_height); 
+		vhdldraw.setColor(Black);
 
-		-- Show the resulting barcode image
-		vhdldraw.show(input_str & "_barcode.ppm");
+		--vhdldraw.fillRectangle(x,y,width height);
+		report(
+			to_string(x_pos) & " " & 
+			to_string(y_pos) & " " & 
+			to_string(quiet_zone) & " " & 
+			to_string(bar_height));
+		vhdldraw.fillRectangle(x_pos, y_pos, quiet_zone, bar_height);
 
-		wait;  -- Wait indefinitely
+		--(from Wikipedia)
+		--A Code 128 barcode has seven sections:
+			--Quiet zone
+			--Start symbol
+			--Encoded data
+			--Check symbol (mandatory)
+			--Stop symbol
+			--Final bar (often considered part of the stop symbol)
+			--Quiet zone
+
+		vhdldraw.show(input_str & "_barcode.ppm"); -- Show the resulting barcode image
+		wait;  
 	end process;
 end architecture;
