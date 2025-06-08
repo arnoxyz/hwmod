b06d10e4fadcf97234a226674e3fcc7f454d29a3
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-08 15:33:53 +0200

    chapter1 barcode, add fill_barcode

--- chapter1/barcode/src/barcode.vhd
+++ chapter1/barcode/src/barcode.vhd
@@ -11,25 +11,39 @@ entity barcode is
 end entity;
 
 architecture arch of barcode is
+	-- Config:
+	constant ACTIVE_CODE : code128_t := CODE_A; -- Select the code that will be generated:
+	constant input_str: string := "AB"; 
+
+
 	-- Stuff I care later about: (make the drawing look nice)
 	-- TODO: adjust drawing window size: Initialize drawing window (having width / 10 as top and bottom margin looks nice,  / 10 works as bar height)
 	-- TODO: adjust windows_width -> determine based on input string, 400 ist just a placeholder
 	-- TODO: adjust windows_height -> Calculate based on window width
 
+	-- barcode stuff
+	signal barcode_data : barcode_t(1 to input_str'length); -- 2 barcode symbols AB
 begin
 	barcode_maker : process
+
 		-- basic drawing stuff init
 		variable vhdldraw : vhdldraw_t;
 		variable y_pos : natural;
 		variable x_pos : natural; 
-
 		-- draw Window
 		variable window_width  : natural := 400; 
 		variable window_height : natural := 400;          
-
-		constant input_str: string := "A"; 
 		constant bar_width : natural := 2; -- defined in wiki (modulo)
 		variable bar_height : natural := window_height;           
+
+		-- fill barcode_data with specific data from input_str
+		procedure fill_barcode_data is
+		begin 
+			for i in input_str'range loop
+				barcode_data(i) <= code128_table(character'pos(input_str(i)));
+			end loop;
+			wait for 1 ns;
+		end procedure;
 
 		-- widths of different bar-zones:
 		constant start_symbol : natural 	:= 11 * bar_width; 
@@ -37,6 +51,14 @@ begin
 		constant quiet_zone   : natural 	:= 15 * bar_width;  
 		constant check_width  : natural 	:= 11 * bar_width;
 		constant stop_width   : natural 	:= 15 * bar_width; 
+
+		-- input a barcode like 11010000100 and draws it accordingly
+		procedure draw_symbol(code : std_ulogic_vector(10 downto 0)) is
+		begin 
+			report(to_string(code));
+			-- vhdldraw.fillRectangle(x_pos, y_pos, quiet_zone, bar_height);
+			-- draw_stats(x_pos,y_pos,quiet_zone,bar_height);
+		end procedure;
 
 		-- for debugging
 		procedure draw_stats(x : natural; y : natural; w : natural; h: natural) is
@@ -47,12 +69,6 @@ begin
 				"width="  & to_string(quiet_zone) & " " & 
 				"height=" & to_string(bar_height));
 		end procedure;
-
-		-- TODO: draw bar 
-			-- (bar-space, bar-space, bar-space)
-			-- Example
-			-- "11010000100",  -- 103 (Start Code A)
-			-- vhdldraw.fillRectangle(x_pos, y_pos, quiet_zone, bar_height);
 
 	begin
 		vhdldraw.init(window_width, window_height); 
@@ -66,11 +82,16 @@ begin
 			draw_stats(x_pos,y_pos,quiet_zone,bar_height);
 
 			--Start symbol
-			-- TODO: add procedure that draws bars according to specification (3 bars width)
 
 			--Encoded data 
+			fill_barcode_data;
+			for i in barcode_data'range loop
+				draw_symbol(barcode_data(i));
+			end loop;
+
 			--Check symbol (mandatory) 
 				--TODO:-> Check digit calculation, 11 Modules per Symbol so check (SumSymbols) % 11 == 0
+
 			--Stop symbol
 			--Final bar (often considered part of the stop symbol)
 			--Quiet zone
