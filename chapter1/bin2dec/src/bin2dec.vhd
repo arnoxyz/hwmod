75a3edba94946404e0057cb10ef78be53c19124a
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-19 22:26:38 +0200

    chapter1 bin2dec, get digits from dec_out

--- chapter1/bin2dec/src/bin2dec.vhd
+++ chapter1/bin2dec/src/bin2dec.vhd
@@ -18,7 +18,9 @@ architecture beh of bin2dec is
 begin
   main : process(bin_in) is
     variable int_local : integer := 0;
+    variable int_local2 : integer := 0;
   begin
+    -- convert bin_in into integer for dec_out
     for i in bin_in'range loop
       if bin_in(i) = '1' then
         int_local := int_local + 2**(i);
@@ -26,7 +28,21 @@ begin
     end loop;
     dec_out <= int_local;
 
-    -- dummy values for now
+    int_local2 := int_local;
+    -- decode the dec_out to bcd format
+    -- report to_string(log10c(int_local));
+    for i in log10c(int_local) downto 0  loop
+        if i/=0 then 
+          -- split int_local in all: 219 -> /100 gets 2, /10 = 1 ...
+          report to_string(i);
+          report to_string(10**(i-1));
+          report to_string(int_local/(10**(i-1)));
+          -- add procedure: decoder of bcd format 
+        else 
+          report to_string(int_local mod 10);
+
+        end if;
+    end loop;
     bcd_out <= (others=>'0');
   end process;
 end architecture;
--- chapter1/bin2dec/tb/bin2dec_tb.vhd
+++ chapter1/bin2dec/tb/bin2dec_tb.vhd
@@ -10,17 +10,17 @@ entity bin2dec_tb is
 end entity;
 
 architecture tb of bin2dec_tb is
-  signal bin_in : std_ulogic_vector(3 downto 0) := (others=>'0');
+  constant WIDTH : integer := 8;
+
+  signal bin_in : std_ulogic_vector(WIDTH-1 downto 0) := (others=>'0');
   signal dec_out : integer;
-  signal bcd_out : std_ulogic_vector(0 downto 0);
+  signal bcd_out : std_ulogic_vector(12 downto 0);
 begin
 
 	stimuli : process
 	begin
 		-- apply your stimulus here
-    bin_in <= "0001";
-    wait for 1 ns;
-    bin_in <= "1101";
+    bin_in <= "11011011";
     wait for 1 ns;
 		report to_string(bin_in) & " is decimal: " & to_string(dec_out) & " is BCD: " & to_string(bcd_out);
 		wait;
