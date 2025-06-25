3d1497e504e9e5b073ffa9b35baba3ec8bad415d
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-25 11:23:47 +0200

    chapter1 illusions ouchi draw background, some rectangles

---
+++ chapter1/illusions/src/illusions.vhd
@@ -0,0 +1,13 @@
+use work.vhdldraw_pkg.all;
+
+entity illusions is
+end entity;
+
+
+architecture arch of illusions is
+begin
+	ouchi : entity work.ouchi;
+	--checkerboard : entity work.checkerboard;
+	--concentric : entity work.concentric;
+	--squarecircle : entity work.squarecircle;
+end architecture;
--- chapter1/illusions/src/ouchi.vhd
+++ chapter1/illusions/src/ouchi.vhd
@@ -9,15 +9,32 @@ begin
 		constant window_size : natural := 800;
 		constant width : natural := 32;
 		constant height : natural := 8;
-		variable vhdldraw : vhdldraw_t;
+		variable draw : vhdldraw_t;
+
+    constant rect_width : natural := 32;
+    constant rect_height : natural := 8;
+    variable rect_x,rect_y : natural := 0;
 
 		-- you might want to add some auxiliary subprograms or constants / variables in here
 	begin
-		vhdldraw.init(window_size);
-
+		draw.init(window_size);
 		-- draw the illusion here
 
-		vhdldraw.show("ouchi.ppm");
+    draw.setColor(Black);
+    draw.fillRectangle(rect_x,rect_y,rect_width,rect_height);
+
+    draw.setColor(White);
+    rect_x := rect_x + rect_width;
+    draw.fillRectangle(rect_x,rect_y,rect_width,rect_height);
+
+    draw.setColor(Black);
+    rect_x := rect_x + rect_width;
+    draw.fillRectangle(rect_x,rect_y,rect_width,rect_height);
+
+
+    report "Hello World!";
+
+		draw.show("ouchi.ppm");
 		wait;
 	end process;
 
