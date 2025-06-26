b3006d941a5ccd806dc8e20efaf2c98829725244
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-26 12:53:56 +0200

    chapter1 illusions ouchi draw frectangle line full window

--- chapter1/illusions/src/ouchi.vhd
+++ chapter1/illusions/src/ouchi.vhd
@@ -6,7 +6,10 @@ end entity;
 architecture arch of ouchi is
 begin
 	process is
-		constant window_size : natural := 800;
+		constant window : natural := 800;
+		constant window_height : natural := 800;
+		constant window_width : natural := 800;
+
 		variable draw : vhdldraw_t;
 
     constant rect_width : natural := 32;
@@ -17,16 +20,22 @@ begin
       variable idx : natural := 0;
     begin
       draw.setColor(Black);
-      while idx < window_size loop
+      while idx < window_width loop
         draw.fillRectangle(idx,y,rect_width,rect_height);
         idx := idx+(2*rect_width);
       end loop;
     end procedure;
 
+    variable idx : integer := 0;
 	begin
 
-		draw.init(window_size);
-    draw_rectangle_line(0);
+		draw.init(window_width, window_height);
+
+    while idx < window_height loop
+      draw_rectangle_line(idx);
+      idx := idx+rect_height;
+    end loop;
+
 		draw.show("ouchi.ppm");
 		wait;
 	end process;
