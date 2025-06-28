7a5a3b4750b1c8a3e3ce88bc352e70e0534ef15a
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-28 14:35:24 +0200

    chapter1 illusions concentric add draw one circle

--- chapter1/illusions/src/concentric.vhd
+++ chapter1/illusions/src/concentric.vhd
@@ -8,12 +8,25 @@ begin
 	process is
 		constant window_width : natural := 840;
 		constant window_height : natural := 480;
-		variable vhdldraw : vhdldraw_t;
+		variable draw : vhdldraw_t;
 
 	begin
-		vhdldraw.init(window_width, window_height);
+		draw.init(window_width, window_height);
 		-- draw the illusion here
-		vhdldraw.show("concentric.ppm");
+    draw.setColor(220-40*0, 220-40*0, 220-40*0);
+    draw.fillCircle(window_width/2,window_height/2, 60-10*0);
+    draw.setColor(220-40*1, 220-40*1, 220-40*1);
+    draw.fillCircle(window_width/2,window_height/2, 60-10*1);
+    draw.setColor(220-40*2, 220-40*2, 220-40*2);
+    draw.fillCircle(window_width/2,window_height/2, 60-10*2);
+    draw.setColor(220-40*3, 220-40*3, 220-40*3);
+    draw.fillCircle(window_width/2,window_height/2, 60-10*3);
+    draw.setColor(220-40*4, 220-40*4, 220-40*4);
+    draw.fillCircle(window_width/2,window_height/2, 60-10*4);
+    draw.setColor(220-40*5, 220-40*5, 220-40*5);
+    draw.fillCircle(window_width/2,window_height/2, 60-10*5);
+		draw.show("concentric.ppm");
+
 		wait;
 	end process;
 
