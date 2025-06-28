a19890102d79bd3002c5be4c2e9129719e5c4e41
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-28 14:54:08 +0200

    chapter1 illusions concentric add draw function

--- chapter1/illusions/src/concentric.vhd
+++ chapter1/illusions/src/concentric.vhd
@@ -11,23 +11,22 @@ begin
 		variable draw : vhdldraw_t;
 
     procedure draw_circle(x : natural; y : natural) is 
+      constant SIZE_CHANGE : natural := 10;
+      constant COLOR_CHANGE : natural := 40;
     begin 
-      report "Hello";
+      for i in 0 to 5 loop
+        -- draw the filled circles
+        draw.setColor(220-COLOR_CHANGE*i, 220-COLOR_CHANGE*i, 220-COLOR_CHANGE*i);
+        draw.fillCircle(x,y, 60-SIZE_CHANGE*i);
+        -- draw the outlines
+        draw.setColor(Black);
+        draw.drawCircle(x,y, 60-SIZE_CHANGE*i);
+      end loop;
     end procedure;
 	begin
 		draw.init(window_width, window_height);
-		-- draw the illusion here
-    --draw_circle(window_width/2, window/height/2);
 
-    for i in 0 to 5 loop
-      -- draw the filled circles
-      draw.setColor(220-40*i, 220-40*i, 220-40*i);
-      draw.fillCircle(window_width/2,window_height/2, 60-10*i);
-      -- draw the outlines 
-      draw.setColor(Black);
-      draw.drawCircle(window_width/2,window_height/2, 60-10*i);
-    end loop;
-
+    draw_circle(window_width/2, window_height/2);
 
 		draw.show("concentric.ppm");
 		wait;
