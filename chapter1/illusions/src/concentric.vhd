6c4e93e8ddf7339901b0241dbbe1d8137bfee8d6
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-28 14:47:53 +0200

    chapter1 illusions concentric add loops for drawing

--- chapter1/illusions/src/concentric.vhd
+++ chapter1/illusions/src/concentric.vhd
@@ -10,32 +10,24 @@ begin
 		constant window_height : natural := 480;
 		variable draw : vhdldraw_t;
 
+    procedure draw_circle(x : natural; y : natural) is 
+    begin 
+      report "Hello";
+    end procedure;
 	begin
 		draw.init(window_width, window_height);
 		-- draw the illusion here
+    --draw_circle(window_width/2, window/height/2);
 
-    -- draw the filled circles
-    draw.setColor(220-40*0, 220-40*0, 220-40*0);
-    draw.fillCircle(window_width/2,window_height/2, 60-10*0);
-    draw.setColor(220-40*1, 220-40*1, 220-40*1);
-    draw.fillCircle(window_width/2,window_height/2, 60-10*1);
-    draw.setColor(220-40*2, 220-40*2, 220-40*2);
-    draw.fillCircle(window_width/2,window_height/2, 60-10*2);
-    draw.setColor(220-40*3, 220-40*3, 220-40*3);
-    draw.fillCircle(window_width/2,window_height/2, 60-10*3);
-    draw.setColor(220-40*4, 220-40*4, 220-40*4);
-    draw.fillCircle(window_width/2,window_height/2, 60-10*4);
-    draw.setColor(220-40*5, 220-40*5, 220-40*5);
-    draw.fillCircle(window_width/2,window_height/2, 60-10*5);
+    for i in 0 to 5 loop
+      -- draw the filled circles
+      draw.setColor(220-40*i, 220-40*i, 220-40*i);
+      draw.fillCircle(window_width/2,window_height/2, 60-10*i);
+      -- draw the outlines 
+      draw.setColor(Black);
+      draw.drawCircle(window_width/2,window_height/2, 60-10*i);
+    end loop;
 
-    -- draw the outlines 
-    draw.setColor(Black);
-    draw.drawCircle(window_width/2,window_height/2, 60-10*0);
-    draw.drawCircle(window_width/2,window_height/2, 60-10*1);
-    draw.drawCircle(window_width/2,window_height/2, 60-10*2);
-    draw.drawCircle(window_width/2,window_height/2, 60-10*3);
-    draw.drawCircle(window_width/2,window_height/2, 60-10*4);
-    draw.drawCircle(window_width/2,window_height/2, 60-10*5);
 
 		draw.show("concentric.ppm");
 		wait;
