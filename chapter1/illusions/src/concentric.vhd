adc1f88693868f76e01d9d1d5a8dd3ec7cfb7bd3
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-28 14:42:43 +0200

    chapter1 illusions concentric add draw the outlines of the circles

--- chapter1/illusions/src/concentric.vhd
+++ chapter1/illusions/src/concentric.vhd
@@ -13,6 +13,8 @@ begin
 	begin
 		draw.init(window_width, window_height);
 		-- draw the illusion here
+
+    -- draw the filled circles
     draw.setColor(220-40*0, 220-40*0, 220-40*0);
     draw.fillCircle(window_width/2,window_height/2, 60-10*0);
     draw.setColor(220-40*1, 220-40*1, 220-40*1);
@@ -25,8 +27,17 @@ begin
     draw.fillCircle(window_width/2,window_height/2, 60-10*4);
     draw.setColor(220-40*5, 220-40*5, 220-40*5);
     draw.fillCircle(window_width/2,window_height/2, 60-10*5);
-		draw.show("concentric.ppm");
 
+    -- draw the outlines 
+    draw.setColor(Black);
+    draw.drawCircle(window_width/2,window_height/2, 60-10*0);
+    draw.drawCircle(window_width/2,window_height/2, 60-10*1);
+    draw.drawCircle(window_width/2,window_height/2, 60-10*2);
+    draw.drawCircle(window_width/2,window_height/2, 60-10*3);
+    draw.drawCircle(window_width/2,window_height/2, 60-10*4);
+    draw.drawCircle(window_width/2,window_height/2, 60-10*5);
+
+		draw.show("concentric.ppm");
 		wait;
 	end process;
 
