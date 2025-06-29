1eaecd4dc192230cbec69e2f0641d1836882697d
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-29 12:42:19 +0200

    chapter1 illusions concentric add black diamond outline

--- chapter1/illusions/src/concentric.vhd
+++ chapter1/illusions/src/concentric.vhd
@@ -10,23 +10,39 @@ begin
 		constant window_height : natural := 480;
 		variable draw : vhdldraw_t;
 
-    procedure draw_circle(x : natural; y : natural) is 
-      constant SIZE_CHANGE : natural := 10;
+    procedure draw_symbol(x : natural; y : natural) is 
+      constant COLOR_INIT : natural := 220;
       constant COLOR_CHANGE : natural := 40;
+
+      constant SIZE_CHANGE : natural := 10;
+      constant CIRCLE_SIZE : natural := 60;
+
+      constant SHIFT: natural := CIRCLE_SIZE;
     begin 
       for i in 0 to 5 loop
         -- draw the filled circles
-        draw.setColor(220-COLOR_CHANGE*i, 220-COLOR_CHANGE*i, 220-COLOR_CHANGE*i);
-        draw.fillCircle(x,y, 60-SIZE_CHANGE*i);
+        draw.setColor(COLOR_INIT-COLOR_CHANGE*i, COLOR_INIT-COLOR_CHANGE*i, COLOR_INIT-COLOR_CHANGE*i);
+        draw.fillCircle(x,y, CIRCLE_SIZE-SIZE_CHANGE*i);
         -- draw the outlines
         draw.setColor(Black);
-        draw.drawCircle(x,y, 60-SIZE_CHANGE*i);
+        draw.drawCircle(x,y, CIRCLE_SIZE-SIZE_CHANGE*i);
       end loop;
+
+      --draw the black diamond outline
+        --top_position    [x,y-CIRCLE_SIZE/2]
+        --bottom_position [x,y+CIRCLE_SIZE/2]
+        --left_position   [x-CIRCLE_SIZE/2,y]
+        --right_position  [x+CIRCLE_SIZE/2,y]
+
+      draw.drawLine(x,y-SHIFT,x-SHIFT,y); --from top position to left
+      draw.drawLine(x,y-SHIFT,x+SHIFT,y); --from top position to right
+      draw.drawLine(x,y+SHIFT,x-SHIFT,y); --from bottom to left
+      draw.drawLine(x,y+SHIFT,x+SHIFT,y); --from bottom to right
     end procedure;
 	begin
 		draw.init(window_width, window_height);
 
-    draw_circle(window_width/2, window_height/2);
+    draw_symbol(window_width/2, window_height/2);
 
 		draw.show("concentric.ppm");
 		wait;
