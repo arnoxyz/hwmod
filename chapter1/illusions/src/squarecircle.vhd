68e0c0808677af7e53543a50ab9da27b0d08585a
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-30 13:43:37 +0200

    chapter1 illusions squarecircle add draw one circle

--- chapter1/illusions/src/squarecircle.vhd
+++ chapter1/illusions/src/squarecircle.vhd
@@ -9,28 +9,38 @@ begin
 		constant window_size : natural := 600;
 		variable draw: vhdldraw_t;
 
+    -- for square
     constant square_size : natural := 50;
     constant square_line_width : natural := 2; 
-    constant space : natural := 10;
-    constant offset : natural := 5;
+    constant square_space_between : natural := 10;
+    constant square_offset : natural := 5;
+    variable square_x : natural := 0 + square_offset;
+    variable square_y : natural := 0 + square_offset;
 
-    variable square_x : natural := 0 + offset;
-    variable square_y : natural := 0 + offset;
+    -- for circle
+    constant circle_size : natural := 30;
+    constant circle_line_width : natural := 4; 
+    constant circle_offset : natural := 60;
+    variable circle_x : natural := 0 + circle_offset;
+    variable circle_y : natural := 0 + circle_offset;
 
 	begin
 		draw.init(window_size);
-		-- draw the illusion here
+		-- draw the squares grid
     draw.setLineWidth(square_line_width);
     for j in 0 to 10 loop
-      square_y := offset + j*(space+square_size);
+      square_y := square_offset + j*(square_space_between+square_size);
       for i in 0 to 10 loop
-        draw.drawSquare(square_x + i*(space+square_size), square_y, square_size);
+        draw.drawSquare(square_x + i*(square_space_between+square_size), square_y, square_size);
       end loop;
-      square_x := 0 + offset;
+      square_x := 0 + square_offset;
     end loop;
+
+		-- draw the circles grid
+    draw.setLineWidth(circle_line_width);
+    draw.drawCircle(circle_x, circle_y, circle_size);
 
 		draw.show("squarecircle.ppm");
 		wait;
 	end process;
-
 end architecture;
