284077c2496facadca0b2ffe3b118da6b8620a88
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-30 13:29:56 +0200

    chapter1 illusions squarecircle add draw one square

--- chapter1/illusions/src/squarecircle.vhd
+++ chapter1/illusions/src/squarecircle.vhd
@@ -9,10 +9,20 @@ begin
 		constant window_size : natural := 600;
 		variable draw: vhdldraw_t;
 
+    constant square_size : natural := 50;
+    constant square_line_width : natural := 2; 
+    constant space : natural := 10;
+    constant offset : natural := 5;
+
+    variable square_x : natural := 0 + offset;
+    variable square_y : natural := 0 + offset;
 
 	begin
 		draw.init(window_size);
 		-- draw the illusion here
+    draw.setLineWidth(square_line_width);
+    draw.drawSquare(square_x, square_y, square_size);
+
 		draw.show("squarecircle.ppm");
 		wait;
 	end process;
