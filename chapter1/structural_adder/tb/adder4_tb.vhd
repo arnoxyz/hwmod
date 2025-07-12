c5880eebef3088b3d2aacc26db9fa65b0fac2327
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-07-12 12:02:13 +0200

    chapter1 structural_adder add testing basic gates

--- chapter1/structural_adder/tb/adder4_tb.vhd
+++ chapter1/structural_adder/tb/adder4_tb.vhd
@@ -7,8 +7,64 @@ entity adder4_tb is
 end entity;
 
 architecture tb of adder4_tb is
-	-- You might want to add some signals
+  signal output1, output2, output3 : std_ulogic;
+  signal a,b : std_ulogic;
+
 begin
+  UUT1 : and_gate
+  port map(
+    a => a,
+    b => b,
+    z => output1
+  );
+
+  UUT2 : xor_gate
+  port map(
+    a => a,
+    b => b,
+    z => output2
+  );
+
+  UUT3 : or_gate
+  port map(
+    a => a,
+    b => b,
+    z => output3
+  );
+
+  testing_gates : process is
+    procedure testing_basic_gates is
+    begin
+      report "start - sim basic gates";
+      a <= '0';
+      b <= '0';
+      wait for 1 ns;
+      report "Input=" & to_string(a) & to_string(b) & " and="& to_string(output1) & " xor=" & to_string(output2) &" or=" & to_string(output3);
+
+      a <= '1';
+      b <= '0';
+      wait for 1 ns;
+      report "Input=" & to_string(a) & to_string(b) & " and="& to_string(output1) & " xor=" & to_string(output2) &" or=" & to_string(output3);
+
+      a <= '0';
+      b <= '1';
+      wait for 1 ns;
+      report "Input=" & to_string(a) & to_string(b) & " and="& to_string(output1) & " xor=" & to_string(output2) &" or=" & to_string(output3);
+
+      a <= '1';
+      b <= '1';
+      wait for 1 ns;
+      report "Input=" & to_string(a) & to_string(b) & " and="& to_string(output1) & " xor=" & to_string(output2) &" or=" & to_string(output3);
+
+      report "done testing basic gates";
+    end procedure;
+
+  begin
+    testing_basic_gates;
+    wait;
+  end process;
+
+
 
 	-- Instantiate the unit under test (adder4)
 
@@ -22,7 +78,7 @@ begin
 		end procedure;
 	begin
 		report "simulation start";
-		
+
 		-- Apply test stimuli
 		test_values(0,0,0);
 
