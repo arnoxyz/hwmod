a3663971504dcf297ece1b582b1337513eca3742
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-07-20 18:50:48 +0200

    chapter2 generic_adder add boilerplate code for fixed value of N=4

--- chapter2/generic_adder/src/generic_adder.vhd
+++ chapter2/generic_adder/src/generic_adder.vhd
@@ -17,5 +17,27 @@ end entity;
 
 
 architecture beh of generic_adder is
+  component adder4 is
+    port (
+      A : in std_ulogic_vector(3 downto 0);
+      B : in std_ulogic_vector(3 downto 0);
+      Cin : in std_ulogic;
+
+      S  : out std_ulogic_vector(3 downto 0);
+      Cout  : out std_ulogic
+    );
+  end component;
+
 begin
+
+-- N=4
+adder4_inst : adder4
+	port map(
+		A => A,
+		B => B,
+		Cin => '0',
+		S  => S,
+		Cout  => open
+	);
+
 end architecture;
--- chapter2/generic_adder/tb/generic_adder_tb.vhd
+++ chapter2/generic_adder/tb/generic_adder_tb.vhd
@@ -9,7 +9,43 @@ entity generic_adder_tb is
 end entity;
 
 architecture bench of generic_adder_tb is
+  constant N : positive := 4;
+	signal A,B,S : std_ulogic_vector(N-1 downto 0);
+  signal cout  : std_ulogic;
+
+  component generic_adder is
+    generic (
+      N : positive := 4
+    );
+    port (
+      A    : in std_ulogic_vector(N-1 downto 0);
+      B    : in std_ulogic_vector(N-1 downto 0);
+      S    : out std_ulogic_vector(N-1 downto 0);
+      Cout : out std_ulogic
+    );
+  end component;
 begin
 
-end architecture;
+  stimulus : process is
+  begin
+    report "start sim";
+    A <= "0001";
+    B <= "0001";
+    wait for 1 ns;
+    report to_string(S);
+    report "end sim";
+    wait;
+  end process;
 
+
+generic_adder_inst : generic_adder
+	generic map (
+		N => N
+	)
+	port map (
+		A    => A,
+		B    => B,
+		S    => S,
+		Cout => cout
+	);
+end architecture;
