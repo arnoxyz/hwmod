368193049d764e18ff159a5b0a73c9cead830bda
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-02 14:16:24 +0200

    chapter2 simple_calc add implementation

--- chapter2/simplecalc/src/simplecalc.vhd
+++ chapter2/simplecalc/src/simplecalc.vhd
@@ -24,5 +24,36 @@ entity simplecalc is
 end entity;
 
 architecture arch of simplecalc is
+  signal store_op1 : std_ulogic;
+  signal store_op2 : std_ulogic;
 begin
+
+  sync : process(clk, res_n) is
+  begin
+    if res_n = '0' then
+      operand1 <= (others=>'0');
+      operand2 <= (others=>'0');
+      store_op1 <= '0';
+      store_op2 <= '0';
+    elsif rising_edge(clk) then
+      store_op1 <= store_operand1;
+      store_op2 <= store_operand2;
+
+      -- detect fallin transition
+      if store_op1 = '1' and store_operand1 = '0' then
+        operand1 <= operand_data_in;
+      elsif store_op2 = '1' and store_operand2 = '0' then
+        operand2 <= operand_data_in;
+      end if;
+    end if;
+  end process;
+
+  comb : process(all) is
+  begin
+    if sub = '0' then
+      result <= std_ulogic_vector(unsigned(operand1)+unsigned(operand2));
+    else
+      result <= std_ulogic_vector(unsigned(operand1)-unsigned(operand2));
+    end if;
+  end process;
 end architecture;
