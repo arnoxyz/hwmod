7716cd189febabd375823199e0d3314d75eb2014
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-05 17:47:01 +0200

    chapter2 lfsr add counter to count cc

--- chapter2/lfsr/tb/lfsr_tb.vhd
+++ chapter2/lfsr/tb/lfsr_tb.vhd
@@ -18,6 +18,9 @@ architecture tb of lfsr_tb is
 
   -- local shift reg to save output data (prdata)
   signal shift_reg : std_ulogic_vector(LFSR_WIDTH-1 downto 0);
+  -- counter to cnt cc
+  signal counter : unsigned(31 downto 0);
+  signal counter_next : unsigned(31 downto 0);
 
   --clk gen
   constant clk_period : time := 2 ns; --50Mhz => 20ns but for sim i just take 2 ns for now
@@ -77,15 +80,23 @@ begin
   begin
     if res_n = '0' then
       shift_reg <= (others=>'0');
+      counter <= (others=>'0');
     elsif rising_edge(clk) then
 
       if load_seed_n = '1' then
+        counter <= counter_next;
+
         shift_reg(0) <= prdata;
         for i in 1 to LFSR_WIDTH-1 loop
           shift_reg(i) <= shift_reg(i-1);
         end loop;
       end if;
     end if;
+  end process;
+
+  comb : process(all) is
+  begin
+    counter_next <= counter + 1;
   end process;
 
 	clk_gen : process is
