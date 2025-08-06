4bbf96f2a79cbd99f75024f333f4bbaea28d2890
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-06 13:46:03 +0200

    chapter2 add test(seed) procedure

--- chapter2/lfsr/tb/lfsr_tb.vhd
+++ chapter2/lfsr/tb/lfsr_tb.vhd
@@ -8,9 +8,9 @@ end entity;
 architecture tb of lfsr_tb is
   --for testcases
 	constant MAX_POLY_8  : std_ulogic_vector(7 downto 0)  := "10111000";
-	--constant POLY_8      : std_ulogic_vector(7 downto 0)  := "10100100";
-	--constant MAX_POLY_16 : std_ulogic_vector(15 downto 0) := "1101000000001000";
-	--constant POLY_16     : std_ulogic_vector(15 downto 0) := "1101001100001000";
+	constant POLY_8      : std_ulogic_vector(7 downto 0)  := "10100100";
+	constant MAX_POLY_16 : std_ulogic_vector(15 downto 0) := "1101000000001000";
+	constant POLY_16     : std_ulogic_vector(15 downto 0) := "1101001100001000";
 
   --generics
 	constant POLYNOMIAL : std_ulogic_vector := MAX_POLY_8;
@@ -21,12 +21,12 @@ architecture tb of lfsr_tb is
   -- counter to cnt cc
   signal counter : unsigned(31 downto 0);
   signal counter_next : unsigned(31 downto 0);
+  signal start_count : std_ulogic := '0';
 
   --clk gen
   constant clk_period : time := 2 ns; --50Mhz => 20ns but for sim i just take 2 ns for now
   signal clk_stop : std_ulogic := '0';
 	signal clk, res_n : std_ulogic := '0';
-
 
   --in
 	signal load_seed_n  : std_ulogic := '1';
@@ -52,11 +52,28 @@ begin
         wait for clk_period;
         res_n <= '1';
     end procedure;
+
+    procedure test(seed_val : integer) is
+    begin
+      set_seed(seed_val);
+
+      --wait to see the start value again then start counting until the register got the start value again
+      wait until to_unsigned(seed_val, LFSR_WIDTH) = unsigned(shift_reg);
+      start_count <= '1';
+      wait until to_unsigned(seed_val, LFSR_WIDTH) = unsigned(shift_reg);
+      start_count <= '0';
+
+      report "seed: " & to_string(seed_val) & ", " & "period: " & to_string(to_integer(counter));
+    end procedure;
+
+
 	begin
     report "start sim";
-    set_seed(100);
-    wait until to_unsigned(100, LFSR_WIDTH) = unsigned(shift_reg);
-    report "found";
+
+
+    for idx in 1 to 255 loop
+      test(idx);
+    end loop;
 
     clk_stop <= '1';
 		wait;
@@ -82,9 +99,10 @@ begin
       shift_reg <= (others=>'0');
       counter <= (others=>'0');
     elsif rising_edge(clk) then
-
       if load_seed_n = '1' then
-        counter <= counter_next;
+        if start_count = '1' then
+          counter <= counter_next;
+        end if;
 
         shift_reg(0) <= prdata;
         for i in 1 to LFSR_WIDTH-1 loop
