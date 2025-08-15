fb88b9b4346a5fd221a208e095174334e137b527
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-15 12:40:12 +0200

    chapter3 pwm add tb boilerplate(stimulus, clk_gen, uut)

--- chapter3/pwm/tb/pwm_signal_generator_tb.vhd
+++ chapter3/pwm/tb/pwm_signal_generator_tb.vhd
@@ -6,13 +6,55 @@ entity pwm_signal_generator_tb is
 end entity;
 
 architecture tb of pwm_signal_generator_tb is
-	constant COUNTER_WIDTH : integer := 8; -- Define the counter width
-	signal clk, res_n, en : std_ulogic := '0';
+  --clk
+  constant CLK_PERIOD : time := 2 ns; --50 MHz => 20 ns
+  signal clk_stop : std_ulogic := '0';
+	signal clk : std_ulogic;
+ signal  res_n : std_ulogic := '0';
+
+  --generics
+	constant COUNTER_WIDTH : integer := 8;
+  --in
+  signal en : std_ulogic := '0';
 	signal value : std_ulogic_vector(COUNTER_WIDTH-1 downto 0) := (others=>'0');
+  --out
 	signal pwm_out : std_ulogic;
+
+  component hpwm_signal_generator is
+    generic (
+      COUNTER_WIDTH : integer := 8
+    );
+    port (
+      clk        : in std_ulogic;
+      res_n      : in std_ulogic;
+      en         : in std_ulogic;
+      value      : in std_ulogic_vector(COUNTER_WIDTH-1 downto 0);
+      pwm_out    : out std_ulogic
+    );
+  end component;
+
 begin
 
-	dut : entity work.pwm_signal_generator
+	stimulus : process
+		procedure check_pwm_signal(low_time, high_time: time) is
+		begin
+      --TODO: implement procedure
+		end procedure;
+
+	begin
+      report "sim start";
+      res_n <= '0';
+      wait for 2*clk_period;
+      res_n <= '1';
+      wait for 10*clk_period;
+
+
+      clk_stop <= '1';
+      report "sim done";
+      wait;
+	end process;
+
+	uut : entity work.pwm_signal_generator
 		generic map (
 			COUNTER_WIDTH => COUNTER_WIDTH
 		)
@@ -24,18 +66,15 @@ begin
 			pwm_out => pwm_out
 		);
 
-	clk_process : process
+	clk_gen: process
 	begin
-		-- TODO: Generate a 1 MHz clock signal
-	end process;
+    clk <= '0';
+    wait for CLK_PERIOD / 2;
+    clk <= '1';
+    wait for CLK_PERIOD / 2;
 
-	stimulus_process : process
-		procedure check_pwm_signal(low_time, high_time: time) is
-		begin
-			-- TODO: Implement this procedure
-		end procedure;
-	begin
-			-- TODO: Implement the test cases
+    if clk_stop = '1' then
+      wait;
+    end if;
 	end process;
-
 end architecture;
