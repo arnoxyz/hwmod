77da381c75483be9e4e67636196cf78eb1b2b854
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-17 13:11:39 +0200

    chapter3 pwm add test on fpga board

--- chapter3/pwm/top_arch.vhd
+++ chapter3/pwm/top_arch.vhd
@@ -1,13 +1,27 @@
-library ieee;
 use ieee.std_logic_1164.all;
 
 architecture top_arch of top is
+	constant COUNTER_WIDTH : integer := 8;
 	signal pwm_out : std_ulogic;
+
+  component pwm_signal_generator is
+    generic (
+      COUNTER_WIDTH : integer := 8
+    );
+    port (
+      clk        : in std_ulogic;
+      res_n      : in std_ulogic;
+      en         : in std_ulogic;
+      value      : in std_ulogic_vector(COUNTER_WIDTH-1 downto 0);
+      pwm_out    : out std_ulogic
+    );
+  end component;
+
 begin
 
-	pwm_signal_generator_inst : entity work.pwm_signal_generator
+	pwm_signal_generator_inst : pwm_signal_generator
 		generic map (
-			COUNTER_WIDTH => 8
+			COUNTER_WIDTH => COUNTER_WIDTH
 		)
 		port map (
 			clk     => clk,
@@ -21,5 +35,4 @@ begin
 
 	-- top-right GPIO pin (GPIO[1] in datasheet/task description)
 	aux(0) <= pwm_out;
-
 end architecture;
