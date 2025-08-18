2d3242c8f7d20dfd107770da308442e57bc5255f
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-18 14:23:43 +0200

    chapter3 running_light add tb boilerplate(stimulus, clk_gen, uut)

--- chapter3/running_light/src/running_light.vhd
+++ chapter3/running_light/src/running_light.vhd
@@ -11,12 +11,28 @@ entity running_light is
 	port (
 		clk      : in std_ulogic;
 		res_n    : in std_ulogic;
-		leds     : out std_ulogic_vector
+		leds     : out std_ulogic_vector(7 downto 0)
 	);
 end entity;
 
-architecture arch of running_light is
+architecture beh of running_light is
+  signal counter : unsigned(31 downto 0);
+  signal counter_nxt : unsigned(31 downto 0);
+
 begin
+  leds <= (others=>'1');
 
+  sync : process(clk, res_n) is
+  begin
+    if res_n = '0' then
+      counter <= (others=>'0');
+    elsif rising_edge(clk) then
+      counter <= counter_nxt;
+    end if;
+  end process;
+
+  comb : process(all) is
+  begin
+    counter_nxt <= counter + 1;
+  end process;
 end architecture;
-
--- chapter3/running_light/tb/running_light_tb.vhd
+++ chapter3/running_light/tb/running_light_tb.vhd
@@ -5,17 +5,45 @@ entity running_light_tb is
 end entity;
 
 architecture arch of running_light_tb is
-	constant LEDS_WIDTH : integer := 8;
 	constant CLK_PERIOD : time := 20 ns;
-	signal stop_clk : boolean := false;
-	signal clk, res_n : std_ulogic;
-	signal leds : std_ulogic_vector(LEDS_WIDTH-1 downto 0);
+	constant STEP_TIME: time := 100 ns;
+
+  --in
+	signal clk_stop : std_ulogic := '0';
+	signal clk : std_ulogic;
+  signal res_n : std_ulogic;
+
+  --out
+	signal leds : std_ulogic_vector(7 downto 0);
+
+  component running_light is
+    generic (
+      STEP_TIME  : time := 1 sec
+    );
+    port (
+      clk      : in std_ulogic;
+      res_n    : in std_ulogic;
+		  leds     : out std_ulogic_vector(7 downto 0)
+    );
+  end component;
 begin
 
+  stimulus : process is
+  begin
+    report "start sim";
+    res_n <= '0';
+    wait until rising_edge(clk);
+    res_n <= '1';
+    wait for 100*clk_period;
 
-	uut : entity work.running_light
+    clk_stop <= '1';
+    report "done sim";
+    wait;
+  end process;
+
+	uut : running_light
 	generic map (
-		STEP_TIME => 100 ns
+		STEP_TIME => STEP_TIME
 	)
 	port map (
 		clk => clk,
@@ -25,7 +53,13 @@ begin
 
 	clk_gen : process is
 	begin
+    clk <= '0';
+    wait for clk_period/2;
+    clk <= '1';
+    wait for clk_period/2;
+
+    if clk_stop = '1' then
+      wait;
+    end if;
 	end process;
-
 end architecture;
-
