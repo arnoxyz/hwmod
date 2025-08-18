3330b892bf56b03804dbd0d57ea3307e84084217
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-18 15:38:10 +0200

    chapter3 running_light add fsm implementation

--- chapter3/running_light/src/running_light.vhd
+++ chapter3/running_light/src/running_light.vhd
@@ -56,36 +56,41 @@ end architecture;
 
 architecture fsm of running_light is
   constant cc_for_step_time : integer := (STEP_TIME / CLK_PERIOD);
-  signal counter : unsigned(31 downto 0);
-  signal counter_nxt : unsigned(31 downto 0);
-  signal leds_internal : unsigned(7 downto 0);
-  signal leds_internal_nxt : unsigned(7 downto 0);
+
+  type fsm_state_t is (OUTPUT_PATTERN);
+
+  type state_reg_t is record
+    --state : fsm_state_t; always in state OUTPUT_PATTERN so omit it for now
+    counter : unsigned(31 downto 0);
+    leds_internal : unsigned(7 downto 0);
+  end record;
+
+  signal s, s_nxt : state_reg_t;
+  constant RESET_VAL : state_reg_t := (counter => (others=>'0'), leds_internal => (7=>'1', others=>'0'));
 
 begin
-  leds <= std_ulogic_vector(leds_internal);
+  leds <= std_ulogic_vector(s.leds_internal);
 
   sync : process(clk, res_n) is
   begin
     if res_n = '0' then
-      counter <= (others=>'0');
-      leds_internal <= (7=>'1', others=>'0');
+      s <= RESET_VAL;
     elsif rising_edge(clk) then
-      counter <= counter_nxt;
-      leds_internal <= leds_internal_nxt;
+      s <= s_nxt;
     end if;
   end process;
 
   comb : process(all) is
   begin
-    counter_nxt <= counter + 1;
-    leds_internal_nxt <= leds_internal;
+    s_nxt <= s;
+    s_nxt.counter <= s.counter + 1;
 
-    if counter >= cc_for_step_time then
-      counter_nxt <= (others=>'0');
-      if leds_internal = 1 then
-        leds_internal_nxt <= (7=>'1', others=>'0');
+    if s.counter >= cc_for_step_time then
+      s_nxt.counter <= (others=>'0');
+      if s.leds_internal = 1 then
+        s_nxt.leds_internal <= (7=>'1', others=>'0');
       else
-        leds_internal_nxt <= shift_right(leds_internal, 1);
+        s_nxt.leds_internal <= shift_right(s.leds_internal, 1);
       end if;
     end if;
   end process;
