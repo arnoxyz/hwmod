40c144b688141b0f7177e7a3221c03bf2b2d98b5
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-20 15:09:54 +0200

    chapter3 bcd_fsm add get_digit10, get_digit1, conversion process done for unsigned data inputs

--- chapter3/bcd_fsm/src/bcd_fsm.vhd
+++ chapter3/bcd_fsm/src/bcd_fsm.vhd
@@ -23,7 +23,7 @@ end entity;
 architecture beh of bcd_fsm is
 	constant DATA_WIDTH : integer := 16;
 	-- TODO: Declare a state register record type
-  type fsm_state is (IDLE, GET_DIGIT_1000, GET_DIGIT_100);
+  type fsm_state is (IDLE, GET_DIGIT_1000, GET_DIGIT_100, GET_DIGIT_10, GET_DIGIT_1);
   signal state : fsm_state;
   signal state_nxt : fsm_state;
   signal cnt : unsigned(6 downto 0);
@@ -36,9 +36,12 @@ architecture beh of bcd_fsm is
 
   signal hex_digit1000_internal : unsigned(6 downto 0);
   signal hex_digit1000_internal_nxt : unsigned(6 downto 0);
-  --signal hex_digit100_internal  : unsigned(6 downto 0);
-  --signal hex_digit10_internal   : unsigned(6 downto 0);
-  --signal hex_digit1_internal    : unsigned(6 downto 0);
+  signal hex_digit100_internal  : unsigned(6 downto 0);
+  signal hex_digit100_internal_nxt  : unsigned(6 downto 0);
+  signal hex_digit10_internal   : unsigned(6 downto 0);
+  signal hex_digit10_internal_nxt   : unsigned(6 downto 0);
+  signal hex_digit1_internal   : unsigned(6 downto 0);
+  signal hex_digit1_internal_nxt   : unsigned(6 downto 0);
 begin
 
 	sync : process(clk, res_n)
@@ -49,12 +52,18 @@ begin
       signed_mode_internal <= '0';
       cnt <= (others => '0');
       hex_digit1000_internal <= (others => '0');
+      hex_digit100_internal <= (others => '0');
+      hex_digit10_internal <= (others => '0');
+      hex_digit1_internal <= (others => '0');
     elsif rising_edge(clk) then
       state <= state_nxt;
       input_data_internal <= input_data_internal_nxt;
       signed_mode_internal <= signed_mode_internal_nxt;
       cnt <= cnt_nxt;
       hex_digit1000_internal <= hex_digit1000_internal_nxt;
+      hex_digit100_internal <= hex_digit100_internal_nxt;
+      hex_digit10_internal <= hex_digit10_internal_nxt;
+      hex_digit1_internal <= hex_digit1_internal_nxt;
     end if;
 	end process;
 
@@ -65,18 +74,23 @@ begin
       signed_mode_internal_nxt <= signed_mode_internal;
       state_nxt <= state;
       cnt_nxt <= cnt;
-
       hex_digit1000_internal_nxt  <= hex_digit1000_internal;
+      hex_digit100_internal_nxt  <= hex_digit100_internal;
+      hex_digit10_internal_nxt  <= hex_digit10_internal;
+      hex_digit1_internal_nxt  <= hex_digit1_internal;
 
-      --just output this for now
       hex_digit1000  <= std_ulogic_vector(hex_digit1000_internal);
-      --hex_digit100   <= std_ulogic_vector(hex_digit100_internal);
-      hex_digit100 <= (others=>'0');
-      --hex_digit10    <= std_ulogic_vector(hex_digit10_internal);
-      hex_digit10 <= (others=>'0');
-      --hex_digit1     <= std_ulogic_vector(hex_digit1_internal);
-      hex_digit1 <= (others=>'0');
+      hex_digit100   <= std_ulogic_vector(hex_digit100_internal);
+      hex_digit10    <= std_ulogic_vector(hex_digit10_internal);
+      hex_digit1     <= std_ulogic_vector(hex_digit1_internal);
+
       hex_sign       <= (others=>'0');
+
+      --CONVERSION-PROCESS:
+      --(INPUT_NUMBER - 1000),  1k_cnt++,   until INPUT_NUMBER < 1000
+      --(INPUT_NUMBER - 100),   100_cnt++,  until INPUT_NUMBER < 100
+      --(INPUT_NUMBER - 10),    10_cnt++,   until INPUT_NUMBER < 10
+      --(INPUT_NUMBER - 1),     1_cnt++,    until INPUT_NUMBER <= 0
 
       case state is
         when IDLE =>
@@ -87,7 +101,6 @@ begin
              (to_integer(input_data_internal) /= to_integer(unsigned(input_data)))) then
             state_nxt <= GET_DIGIT_1000;
           end if;
-
         when GET_DIGIT_1000 =>
           --Now implementing the signed_mode='0' case so the input_data is interpreted as unsigned
           if to_integer(input_data_internal) > 1000 then
@@ -99,16 +112,36 @@ begin
             cnt_nxt <= (others=>'0'); --reset cnt
             state_nxt <= GET_DIGIT_100; --go to the next state -> GET_DIGIT_100
           end if;
-
         when GET_DIGIT_100 =>
-            report "get digit 100...";
-
-          --START-CONVERSION:
-          --(INPUT_NUMBER - 1000),  1k_cnt++,   until INPUT_NUMBER < 1000
-          --(INPUT_NUMBER - 100),   100_cnt++,  until INPUT_NUMBER < 100
-          --(INPUT_NUMBER - 10),    10_cnt++,   until INPUT_NUMBER < 10
-          --(INPUT_NUMBER - 1),     1_cnt++,    until INPUT_NUMBER <= 0
-          report "In START Process";
+          if to_integer(input_data_internal) > 100 then
+            input_data_internal_nxt <= to_unsigned(to_integer(input_data_internal)-100, DATA_WIDTH);
+            cnt_nxt <= cnt+1; --cnt++
+            --stay in same state, omit => because of default assignment at the start
+          else
+            hex_digit100_internal_nxt  <= cnt; --cnt yields the hex_digit1000
+            cnt_nxt <= (others=>'0'); --reset cnt
+            state_nxt <= GET_DIGIT_10; --go to the next state -> GET_DIGIT_100
+          end if;
+        when GET_DIGIT_10 =>
+          if to_integer(input_data_internal) > 10 then
+            input_data_internal_nxt <= to_unsigned(to_integer(input_data_internal)-10, DATA_WIDTH);
+            cnt_nxt <= cnt+1; --cnt++
+            --stay in same state, omit => because of default assignment at the start
+          else
+            hex_digit10_internal_nxt  <= cnt; --cnt yields the hex_digit1000
+            cnt_nxt <= (others=>'0'); --reset cnt
+            state_nxt <= GET_DIGIT_1; --go to the next state -> GET_DIGIT_100
+          end if;
+        when GET_DIGIT_1 =>
+          if to_integer(input_data_internal) > 0 then
+            input_data_internal_nxt <= to_unsigned(to_integer(input_data_internal)-1, DATA_WIDTH);
+            cnt_nxt <= cnt+1; --cnt++
+            --stay in same state, omit => because of default assignment at the start
+          else
+            hex_digit1_internal_nxt  <= cnt;
+            cnt_nxt <= (others=>'0'); --reset cnt
+            state_nxt <= IDLE; --go to the next state
+          end if;
         end case;
 	end process;
 end architecture;
